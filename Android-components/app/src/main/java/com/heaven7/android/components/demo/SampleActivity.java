90b76c05d9fb251794232332444520ea99acc2d6
LinghtSun <532278976@qq.com>
2017-07-17 21:37:15 +0800

    init

---
+++ Android-components/app/src/main/java/com/heaven7/android/components/demo/SampleActivity.java
@@ -0,0 +1,13 @@
+package com.heaven7.android.components.demo;
+
+import java.util.List;
+
+/**
+ * Created by heaven7 on 2017/5/28.
+ */
+public class SampleActivity extends AbsMainActivity {
+    @Override
+    protected void addDemos(List<ActivityInfo> list) {
+
+    }
+}
